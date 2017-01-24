99f31fb34dc2d45756ee2af2ec5c3b54dcbf4fc4
admin <1395258209@qq.com>
2017-01-24 20:52:35 +0800

    创建一个聊天项目

---
+++ IMChatAndSeeding/IMChat/IMChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IMChat
+//
+//  Created by admin on 17/1/24.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IMChatAndSeeding/IMChat/IMChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IMChat
+//
+//  Created by admin on 17/1/24.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
